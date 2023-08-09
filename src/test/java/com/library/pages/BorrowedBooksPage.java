44ac62a5c59a76a08f7218423a59ab4981ce60c5
Lorena_Chedid <lpperez06@gmail.com>
2023-08-09 09:55:29 -0400

    Updated Ft 1 & 7

--- src/test/java/com/library/pages/BorrowedBooksPage.java
+++ src/test/java/com/library/pages/BorrowedBooksPage.java
@@ -10,4 +10,18 @@ public class BorrowedBooksPage extends BasePage{
 
     @FindBy(xpath = "//tbody//td[2]")
     public List<WebElement> allBorrowedBooksName;
+
+    @FindBy(xpath = "//table/tbody/tr/td[1]/a")
+    public WebElement borrowBook;
+
+    @FindBy(xpath = "//td[6][.='NOT RETURNED ']/../td/a")
+    public WebElement returnBook;
+
+    @FindBy(xpath = "//td[6][.='NOT RETURNED ']/preceding-sibling::td[3]")
+    public WebElement checkedOutBookBorrowedDate;
+
+    @FindBy(xpath = "//td[6][.='NOT RETURNED ']/preceding-sibling::td[4]")
+    public WebElement checkedOutBook;
+
 }
+
